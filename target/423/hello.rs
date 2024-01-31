71086b00438caa89a9b0d8022cf5cd8de62a18d0
Concerned Student <not provided>
2024-01-31 13:30:06 -0700

    Update Wed Jan 31 01:30:06 PM MST 2024

--- target/423/hello.rs
+++ target/423/hello.rs
@@ -1,5 +1,5 @@
    fn main() {
-      println!("hello,\t{}! ", &(f("golly", 12))[..]);
+      println!("hello,\t{}! ", &(f("golly", 121212121212121212121212121212121212))[..]);
    }
 fn f(s : &str, i : i64) -> String {
    return format!("{} you {}", s, i);
