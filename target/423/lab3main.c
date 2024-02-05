7f0d758acfd4e6d8ec01eb9de0124d5eac242fee
Concerned Student <not provided>
2024-02-05 16:40:07 -0700

    Update Mon Feb  5 04:40:07 PM MST 2024

--- target/423/lab3main.c
+++ target/423/lab3main.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 extern FILE *yyin;
-main(int argc, char *argv[])
+
+int yyparse();
+int main(int argc, char *argv[])
 {
+   int i;
    yyin = fopen(argv[1], "r");
    if (yyin==NULL) {
      fprintf(stderr, "usage: lab3 filename\n");
      exit(1);
    }
+   i = yyparse();
+   printf("yyparse returns %d\n", i);
+   exit(i);
+   }
+
+int yyerror(char *s) {
+   fprintf(stderr, "%s\n", s); exit(1);
+}
