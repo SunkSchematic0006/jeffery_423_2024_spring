30048e04fddd100e95ad4d00f831e211e4aab7dd
Concerned Student <not provided>
2024-02-06 11:40:07 -0700

    Update Tue Feb  6 11:40:07 AM MST 2024

--- target/423/ironymain.c
+++ target/423/ironymain.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 extern FILE *yyin;
+extern int yydebug;
 
 int yyparse();
 int main(int argc, char *argv[])
 {
    int i;
+   yydebug = 1;
    yyin = fopen(argv[1], "r");
    if (yyin==NULL) {
      fprintf(stderr, "usage: lab3 filename\n");
